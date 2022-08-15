8e961c5b7197d06bc2b54c04abd10ab2d820557d
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-08-15 13:05:38 +0200

    add delete handler to icon

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addBtns = document.querySelectorAll(".add-btn:not(.solid)");
 const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
+const deleteBtn = document.querySelectorAll(".icon-delete");
 // Item Lists
 const listColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
@@ -61,7 +62,7 @@ function createItemEl(columnEl, column, item, index) {
   listEl.classList.add("drag-item");
   listEl.innerHTML = `<span>${item}</span> <div class='icons'><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 icon-edit icon-edit-${column}-${index}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-</svg><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 icon-delete icon-delete-${column}-${index}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+</svg><svg xmlns="http://www.w3.org/2000/svg" class="icon-delete icon-delete-${column}" id='${index}' onclick='deleteItemHamndler(event)'fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
 </svg></div>`;
   listEl.draggable = true;
@@ -221,6 +222,17 @@ function hideInputBox(column) {
   addItemContainers[column].style.display = "none";
 
   addToColumn(column);
+}
+
+//delete icon handler
+function deleteItemHamndler(e) {
+  const classInLetters = e.target
+    .closest(".icon-delete")
+    .classList[1].split("");
+  const column = classInLetters[classInLetters.length - 1];
+  const icon = e.target.closest(".icon-delete").id;
+  listArrays[column].splice(icon, 1);
+  updateDOM();
 }
 
 //On load
