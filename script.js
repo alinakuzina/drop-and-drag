1a2d77defb77bf4f9c409dda69564c6c99012a7b
alinakuzina <kuzina.lg.4.1@gmail.com>
2022-08-16 14:29:13 +0200

    removing free spaces in tasks text

--- script.js
+++ script.js
@@ -62,10 +62,11 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
+  let newItem = item.replace(/\r?\n/g, "").trim();
   // List Item
   const listEl = document.createElement("li");
   listEl.classList.add("drag-item");
-  listEl.innerHTML = `<span class='text-item'>${item}</span> <div class='icons'><svg xmlns="http://www.w3.org/2000/svg" class="icon-edit icon-edit-${column}" id='${index}'  onclick='editItemHamndler(event)'fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  listEl.innerHTML = `<span class='text-item'>${newItem}</span> <div class='icons'><svg xmlns="http://www.w3.org/2000/svg" class="icon-edit icon-edit-${column}" id='${index}'  onclick='editItemHamndler(event)'fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
 </svg><svg xmlns="http://www.w3.org/2000/svg" class="icon-delete icon-delete-${column}" id='${index}' onclick='deleteItemHamndler(event)'fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
@@ -146,7 +147,9 @@ function updateItem(id, column) {
     if (selectedColumnEl[id].textContent.length <= 0) {
       selectedArray.splice(id, 1);
     } else {
-      selectedArray[id] = selectedColumnEl[id].textContent;
+      selectedArray[id] = selectedColumnEl[id].textContent
+        .replace(/\r?\n/g, "")
+        .trim();
     }
     console.log(selectedArray);
 
